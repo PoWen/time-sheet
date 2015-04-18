ee3c7010c66ad7dd292da4340b2abb0d4efd2db4
Charles <charles@ragic.com>
2015-04-19 02:24:08 +0800

    use angular-ui-grid to edit, insert data

--- public/js/data-members.js
+++ public/js/data-members.js
@@ -2,14 +2,71 @@
 
 /* globals angular */
 
-var app = angular.module('app', []);
+var app = angular.module('app', ['ui.grid', 'ui.grid.edit', 'ui.grid.rowEdit', 'ui.grid.cellNav']);
 
-app.controller('DataCtrl', ['$scope', '$http', function ($scope, $http) {
+app.controller('DataCtrl', ['$scope', '$http', '$q', '$interval', 'uiGridConstants', function ($scope, $http, $q, $interval, uiGridConstants) {
     $scope.members = [];
+    $scope.newMember = {};
+
+    // $scope.headers = [
+    //         { name: 'name', displayName: '姓名'},
+    //         { name: 'jobTitle', displayName: '職稱'},
+    // ];
+
+    $scope.gridOptions = {
+        enableFiltering: true,
+        columnDefs: $scope.headers
+    };
+    $scope.gridOptions.onRegisterApi = function (gridApi) {
+        $scope.gridApi = gridApi;
+        gridApi.rowEdit.on.saveRow($scope, $scope.saveRow);
+    };
+
+    $scope.saveRow = function (entry) {
+        var promise = $http.post('/api/members', entry).then(function (res) {
+            var key;
+            for (key in res.data.data) {
+                entry[key] = res.data.data[key];
+            }
+
+            if (res.data.isNew) {
+                addRowForNewMember();
+            }
+        });
+
+        $scope.gridApi.rowEdit.setSavePromise( entry, promise );
+    };
+
+    $scope.$watch('gridOptions.columnDefs', function (newValue, oldValue) {
+        //for hide _id field
+        if (newValue === oldValue) return;
+
+        $scope.gridOptions.columnDefs.forEach(function (col) {
+            if (col.name === '_id') {
+                col.visible = false;
+            }
+        });
+        $scope.gridApi.core.notifyDataChange( uiGridConstants.dataChange.COLUMN );
+
+        // another way to do it
+        // var idIndex;
+        // $scope.gridOptions.columnDefs.forEach(function (col, index) {
+        //     if (col.name === '_id') {
+        //         idIndex = index;
+        //     }
+        // });
+        // $scope.gridOptions.columnDefs.splice(idIndex, 1);
+    });
+
+    var addRowForNewMember = function () {
+        $scope.newMember = { };
+        $scope.members.push($scope.newMember);
+    };
 
     $http.get('/api/members').then(function (res) {
-        var members = res.data;
-        $scope.members = members;
+        $scope.members = res.data;
+        addRowForNewMember();
+        $scope.gridOptions.data = $scope.members;
     });
 
 }]);
--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,8 @@
 
 var debug = require('debug')('routes');
 var express = require('express');
+var bodyparser = require('body-parser');
+
 var router = express.Router();
 
 var exportCsv = require('../lib/export-csv.js');
@@ -26,6 +28,28 @@ router.get('/api/:model', function (req, res) {
 router.get('/export/:model', function (req, res) { 
     var Model = dbManager.getDb().model(req.params.model);
     exportCsv(Model, res);
+});
+
+router.use(bodyparser.json());
+router.post('/api/:model', function (req, res) {
+    var Model = dbManager.getDb().model(req.params.model);
+
+    console.log('receive', req.body);
+    var options = {
+        new: true,
+        upsert: false,
+    };
+
+    if (req.body._id === undefined) {
+        Model.create(req.body, function (err, doc) {
+            res.json({ isNew: true, data: doc });
+        });
+    } else {
+        Model.findOneAndUpdate({ _id: req.body._id }, req.body, options, function (err, doc) {
+            res.json({ isNew: false, data: doc });
+        });
+    }
+
 });
 
 module.exports = router;
